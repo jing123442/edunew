d3df8d08e5542ce5a603d7b9c69292d54137c2b6
whyymj1987623 <wuhao-v@jbinfo.cn>
2018-10-23 14:27:03 +0800

    武浩提交mock

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="hello">
- 
   </div>
 </template>
 
 <script>
-import creatNewStudent from './creatNewStudent'
-export default {
-  name: 'HelloWorld',
- 
-  data () {
-    return {
-      
+  import creatNewStudent from './creatNewStudent'
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {}
+    },
+    mounted() {
+      console.log('ppppp')
+      this.$axios.get('http://mytestdata/getAllClass').then(function(res) {
+        console.log(res)
+      })
+     
     }
   }
-}
 </script>
 
 
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,47 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// The Vue build version to load with the `import` command (runtime-only or
+// standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css';
+Vue.prototype.$axios = axios;
+
+if (process.env.NODE_ENV == 'development') { //拦截请求模拟数据
+  var Mock = require('mockjs');
+  var dataroutes = require('../static/testData/getAllClass.js');
+  let modules = Object.keys(dataroutes); //获取全部路由
+  let routes = Object
+    .keys(dataroutes)
+    .map((item) => item.toLowerCase()); //获取全部路由
+  let routeIndex = -1;
+  Mock.mock(/http:\/\/mytestdata\/.*/, function (options) {
+    var url = options
+      .url
+      .toLowerCase();
+    var route = routes.filter((item, index) => {
+      let result = false;
+      result = url.indexOf(item) != -1;
+      if (result) {
+        routeIndex = index;
+      }
+      return result;
+    });
+    if (route.length > 0) {
+      var template = dataroutes[modules[routeIndex]]
+      return Mock.mock(template)
+    }
+
+  })
+  Mock.mock(/http:\/\/ppp\/.*/, function (options) {
+    console.log(options)
+    return {"classList": 123}
+  })
+}
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+new Vue({el: '#app', router, components: {
+    App
+  }, template: '<App/>'})
